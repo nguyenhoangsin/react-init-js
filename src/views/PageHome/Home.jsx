d3a16cb3229ee9e0f674cba67f152e0aceba2ab1
Sin <sinnh@bap.jp>
2019-04-22 09:04:52 +0700

    v1 - Add module css with scss

--- src/views/PageHome/Home.jsx
+++ src/views/PageHome/Home.jsx
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react';
+import styles from './Home.module.scss';
 
 class Home extends PureComponent {
   render() {
     return (
-      <h1>Home</h1>
+      <h1 className={styles.testModuleCss}>Home</h1>
     );
   }
 }
